fb7a47feecc916c5c5692ac86ea8beb90349a03d
宋亮亮 <songliangliang@zto.cn>
2016-11-11 06:20:24 +0800

    1、修改master

--- app/src/main/java/com/zto/myapplication/MainActivity.java
+++ app/src/main/java/com/zto/myapplication/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            return false;
         }
 
         return super.onOptionsItemSelected(item);
